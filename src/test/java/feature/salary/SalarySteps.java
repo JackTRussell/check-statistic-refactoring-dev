39397d31d3fa4486530223b8db4a77af287e04e8
JackTRussell <JackTRussell@users.noreply.github.com>
2017-03-21 16:44:55 +0200

    Update SalarySteps.java

--- src/test/java/feature/salary/SalarySteps.java
+++ src/test/java/feature/salary/SalarySteps.java
@@ -37,12 +37,13 @@ public class SalarySteps {
 	public void the_boss_increases_the_salary_for_the_user_with_id_id_by_(int arg1) throws Throwable {
 		//TODO: add Table
 	}
-	@When("^the boss increases the salary for the employee with name")
-	public void the_boss_increases_the_salary_for_the_employee_with_id_by(final int id, final int increaseInPercent) throws Throwable {
-		manager.increaseSalary(id, increaseInPercent);
-	}	
 	@When("^i want save this string to Git$")
-	public void the_boss_increases_the(final int id, final int increaseInPercent) throws Throwable {
-		manager.increaseSalary(id, increaseInPercent);
+	public void save_changes(int arg1) throws Throwable {
+		//TODO: add Table
+	}
+	
+	@When("^this also must be re-stored on GIT!!!!$")
+	public void save_changes__dsac(int arg1) throws Throwable {
+		//TODO: add Table
 	}
 }
